40815c67d6c7d79f9d3c9c5c0d107964f34babb7
pritsen <prithwiraj.sen@cascnet.com>
2021-06-25 19:36:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
 from flask import Flask
-import pandas as pd
-from tqdm import tqdm
-from sqlalchemy import create_engine
-import urllib
-from azure.storage.blob import BlobServiceClient
-from fbodatfunc import chunker
-from fbodatfunc import connectdb
-import time
 
 app = Flask(__name__)
 
