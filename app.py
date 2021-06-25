8b487ae9bd4eb53123cd5a05e94509b38cf75753
pritsen <prithwiraj.sen@cascnet.com>
2021-06-25 19:20:34 +0530

    function file
    Add Function File

--- app.py
+++ app.py
@@ -4,17 +4,10 @@ from tqdm import tqdm
 from sqlalchemy import create_engine
 import urllib
 from azure.storage.blob import BlobServiceClient
+from fbodatfunc import chunker
+from fbodatfunc import connectdb
 
 app = Flask(__name__)
-def chunker(seq, size):
-    # from http://stackoverflow.com/a/434328
-    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
-
-def connectdb():
-    quoted = urllib.parse.quote_plus(
-     "Driver={ODBC Driver 17 for SQL Server};Server=tcp:fbonextdb.database.windows.net,1433;Database=FBONextDB;Uid=fbodbadmin;Pwd={!1BHeN3?rt<q78i2};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30")
-    engine = create_engine('mssql+pyodbc:///?odbc_connect={}'.format(quoted))
-    return engine
 
 @app.route("/compdata")
 def compdata():
---
+++ fbodatfunc.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from tqdm import tqdm
+from sqlalchemy import create_engine
+import urllib
+
+def chunker(seq, size):
+    # from http://stackoverflow.com/a/434328
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
+def connectdb():
+    quoted = urllib.parse.quote_plus(
+     "Driver={ODBC Driver 17 for SQL Server};Server=tcp:fbonextdb.database.windows.net,1433;Database=FBONextDB;Uid=fbodbadmin;Pwd={!1BHeN3?rt<q78i2};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30")
+    engine = create_engine('mssql+pyodbc:///?odbc_connect={}'.format(quoted))
+    return engine
