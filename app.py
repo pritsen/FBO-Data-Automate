5ecc23672552808cf3d1d89de6fd8f722c051ea3
pritsen <prithwiraj.sen@cascnet.com>
2021-06-26 15:04:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,8 +38,4 @@ def compdata():
 
 @app.route("/test")
 def hello():
-    return "Hello, Test!"
-
-@app.route("/test2")
-def hello2():
-    return "Hello, Test2!"
+    return "API Test Successful!"
