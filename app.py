02b856832be507b3f7bca069a5826b23b48648e9
pritsen <prithwiraj.sen@cascnet.com>
2021-06-27 00:38:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 from sqlalchemy import create_engine
 import urllib
 from azure.storage.blob import BlobServiceClient
+from azure.storage.blob import BaseBlobService
 #from azure.storage.blob import blob_service
 from fbodatfunc import chunker
 from fbodatfunc import connectdb
@@ -17,20 +18,22 @@ app = Flask(__name__)
 def compdata():
     try:
         STORAGEACCOUNTURL= "https://fbostoracct.blob.core.windows.net/"
+        account_name="fbostoracct"
+        account_key="qcNEwmBQtcOOQAgDdHLwpC02urawMOxpYNHvkUtK4FKiWcuuAxCiZpLMrD1uE5QQl/etZoxk85cCICGsznlBCA=="
         STORAGEACCOUNTKEY= "qcNEwmBQtcOOQAgDdHLwpC02urawMOxpYNHvkUtK4FKiWcuuAxCiZpLMrD1uE5QQl/etZoxk85cCICGsznlBCA=="
-        #LOCALFILENAME= "Opportunity-2.csv"
         CONTAINERNAME= "fbo-blob"
         BLOBNAME= "OpportunityData-2.csv"
-        #download from blob
+        LOCALFILENAME = "test.csv"
         t1=time.time()
         blob_service_client_instance = BlobServiceClient(account_url=STORAGEACCOUNTURL, credential=STORAGEACCOUNTKEY)
-        #blob_client_instance = blob_service_client.download_blob(CONTAINERNAME, BLOBNAME, snapshot=None)
-        #with open(LOCALFILENAME, "wb") as my_blob:
-            #blob_data = blob_client_instance.download_blob()
-            #blob_data.readinto(my_blob)
-        #t2=time.time()
-        blobstring = blob_service_client_instance.get_blob_to_text(CONTAINERNAME,BLOBNAME).content
-        df=pd.read_csv(StringIO(blobstring))
+        blob_client_instance = blob_service_client_instance.get_blob_client(CONTAINERNAME, BLOBNAME, None)
+        with open(LOCALFILENAME, "wb") as my_blob:
+            blob_data = blob_client_instance.download_blob()
+            blob_data.readinto(my_blob)
+            t2=time.time()
+        print(("It takes %s seconds to download "+BLOBNAME) % (t2 - t1))
+        df=pd.read_csv(LOCALFILENAME)
+        #print(df)
         return df
     except Exception as e:
         return "Unable to read file" + e
