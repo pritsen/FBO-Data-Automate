f28c723bd6c9981db42c765224076d4e41afb448
pritsen <prithwiraj.sen@cascnet.com>
2021-06-26 22:51:18 +0530

    Update

--- test.py
+++ test.py
@@ -3,24 +3,20 @@ import pandas as pd
 from tqdm import tqdm
 from sqlalchemy import create_engine
 import urllib
-from azure.storage.blob import BlobServiceClient
+import azure.storage.blob
+#from azure.storage.blob import BlobServiceClient
 #from azure.storage.blob import blob_service
 from fbodatfunc import chunker
 from fbodatfunc import connectdb
 import time
 from io import StringIO
 
-
-app = Flask(__name__)
-
-@app.route("/oppdata")
-def compdata():
-    try:
+try:
         STORAGEACCOUNTURL= "https://fbostoracct.blob.core.windows.net/"
         STORAGEACCOUNTKEY= "qcNEwmBQtcOOQAgDdHLwpC02urawMOxpYNHvkUtK4FKiWcuuAxCiZpLMrD1uE5QQl/etZoxk85cCICGsznlBCA=="
         #LOCALFILENAME= "Opportunity-2.csv"
         CONTAINERNAME= "fbo-blob"
-        BLOBNAME= "OpportunityData-2.csv"
+        BLOBNAME= "Opportunity-2.csv"
         #download from blob
         t1=time.time()
         blob_service_client_instance = BlobServiceClient(account_url=STORAGEACCOUNTURL, credential=STORAGEACCOUNTKEY)
@@ -31,11 +27,6 @@ def compdata():
         #t2=time.time()
         blobstring = blob_service_client_instance.get_blob_to_text(CONTAINERNAME,BLOBNAME).content
         df=pd.read_csv(StringIO(blobstring))
-        return df
-    except Exception as e:
-        return "Unable to read file" + e
-
-
-@app.route("/test")
-def hello():
-    return "API Test Successful!"
+        print(df)
+except Exception as e:
+        print("Unable to read file", e)
